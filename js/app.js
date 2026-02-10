b23f40e0c3812797fdb6d946adf7a8f99e3d2d39
“sumit” <sumit.ducat@gmail.com>
2026-02-10 21:02:26 +0530

    four

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const API="https://fakestoreapi.com/products";
 const prodData=async ()=>{
     const res=await fetch(API);
+    if(!res.ok) throw new Error("Network responce was not ok!")
     return res.json();
 }
 let products=[];
@@ -19,13 +20,31 @@ let products=[];
   <div class="card-body">
     <h5 class="card-title">${prod.title}</h5>
     <p class="card-text">Price : $ ${prod.price}</p>
-    <a href="#" class="btn btn-primary">More Details</a>
+    <button class="btn btn-primary" onclick="showDetails(${prod.id})">More Details</button>
   </div>
 </div>
             </div>
             `
         ).join("");
         list.innerHTML=cards;
+        const modal=new bootstrap.Modal(document.getElementById("productModal"));
+        let currentProduct=null;
+       window.showDetails=function(id){
+        console.log("Hello")
+        const p=products.find(x=> x.id===id)
+        if(!p) return;
+        currentProduct=p;
+        document.getElementById("modalTitle").innerText=p.title
+        document.getElementById("modalDesc").innerText=p.description
+        document.getElementById("modalPrice").innerText="$" + p.price
+        const img1=document.getElementById("modalImg");
+        img1.src=p.image;
+        img1.alt=p.title;
+        modal.show();
+        }
+
+
+// Product Details Modal
    }
    catch(err){
      console.log(err)
