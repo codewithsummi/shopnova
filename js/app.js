e5989973a4907b8059bbc4dcd9e8a4e0eaab11eb
“sumit” <sumit.ducat@gmail.com>
2026-02-10 20:34:24 +0530

    second1

--- js/app.js
+++ js/app.js
@@ -9,6 +9,23 @@ let products=[];
    try{
         products=await prodData();
         console.log(products);
+        // render products 
+        const list=document.getElementById("productList");
+        const cards=products.map(prod=> 
+            `
+            <div class="col-md-4">
+              <div class="card" style="width: 18rem;">
+  <img src="${prod.image}" class="card-img-top" alt="..." width="200" height="150">
+  <div class="card-body">
+    <h5 class="card-title">${prod.title}</h5>
+    <p class="card-text">Price : $ ${prod.price}</p>
+    <a href="#" class="btn btn-primary">More Details</a>
+  </div>
+</div>
+            </div>
+            `
+        ).join("");
+        list.innerHTML=cards;
    }
    catch(err){
      console.log(err)
